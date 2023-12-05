d47a3c9dffa444548f43b597c92e3c56f2ca7a32
breathin <breathin91@naver.com>
2023-12-05 11:15:01 +0900

    ?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import {useContainerQuery} from 'react-container-query';
 
 const query = {
